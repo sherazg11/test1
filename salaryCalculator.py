ac29c0545bae47d5a758c03bbfff37fa523048ea
sherazg11 <sherazg924@gmail.com>
2024-03-23 22:04:31 +0500

    Added Average Salary Calculator

---
+++ salaryCalculator.py
@@ -0,0 +1,10 @@
+print("Calculate the Average of 3 Salaries")
+name1 = input("Enter Name: ")
+salary1 = input("Enter 1st Salary: ")
+salary2 = input("Enter 2nd Salary: ")
+salary3 = input("Enter 3rd Salary: ")
+
+print("Name: ", name1)
+sum = int(salary1)+int(salary2)+int(salary3)
+average = sum/3
+print("Average Salary is:", average)
