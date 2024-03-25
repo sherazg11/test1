cec8078f668a4edc1ea1b48bf7f1dd8ebfc14846
sherazg11 <sherazg924@gmail.com>
2024-03-25 23:18:06 +0500

    Team Formation Checker

---
+++ teamFormation.py
@@ -0,0 +1,7 @@
+boys = ['ahmad', 'anas', 'shami']
+girls = ['Momina', 'saba', 'sana', 'sadia']
+
+if len(boys)==len(girls):
+    print("GOod team formation")
+else:
+    print("Team Cant Be Formed")
